5c8585648817e4795bc6dd55249a9a4c775d2a5f
양수현( Soo Hyun Yang) <111266513+YangSooHyun0@users.noreply.github.com>
2023-08-15 20:12:36 +0900

    사용자 로그인 엔드포인트 기능구현 완료
    * feat: 사용자 로그인 엔드포인트 기능구현 (#4)

--- src/main/java/com/board/wanted/user/controller/UserController.java
+++ src/main/java/com/board/wanted/user/controller/UserController.java
@@ -6,11 +6,13 @@ import com.board.wanted.user.service.UserService;
 import com.board.wanted.user.model.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+
 @Slf4j
 @RequiredArgsConstructor
 @RestController
@@ -25,5 +27,18 @@ public class UserController {
         userService.saveUser(userDTO);
 
         return ResponseEntity.ok(ApiResponse.success(null));
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<ApiResponse.Result<String>> signIn(@RequestBody @Valid UserRequest.SignInDTO signInDTO, Errors errors) {
+
+        String jwt = userService.signIn(signInDTO);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.AUTHORIZATION, jwt);
+
+        log.info("{}", ResponseEntity.ok().headers(headers).body(null));
+
+        return ResponseEntity.ok().headers(headers).body(null);
     }
 }
