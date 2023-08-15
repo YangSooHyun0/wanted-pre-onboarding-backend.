01bd5fc418c1122b0f0f3f24831af19c96a1d9f7
yangsoohyun <cendy1234@naver.com>
2023-08-15 19:48:02 +0900

    feat : 다양한 에러 메시지를 정의하기 위한 'ErrorMessage' 클래스를 추가 (#3)

---
+++ src/main/java/com/board/wanted/_core/errors/ErrorMessage.java
@@ -0,0 +1,19 @@
+package com.board.wanted._core.errors;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ErrorMessage {
+
+    public static final String UN_AUTHORIZED = "인증되지 않았습니다.";
+    public static final String FORBIDDEN = "권한이 없습니다.";
+
+    public static final String TOKEN_UN_AUTHORIZED = "검증되지 않은 토큰입니다.";
+    public static final String TOKEN_EXPIRED = "만료된 토큰입니다.";
+    public static final String TOKEN_VERIFICATION_FAILED = "토큰 검증에 실패했습니다.";
+
+    public static final String EMPTY_DATA_TO_SIGNUP = "회원 가입을 위한 데이터가 존재하지 않습니다.";
+    public static final String EMPTY_DATA_TO_SIGNIN = "로그인을 위한 데이터가 존재하지 않습니다.";
+    public static final String LOGIN_FAILED = "회원 정보가 존재하지 않습니다.";
+}
