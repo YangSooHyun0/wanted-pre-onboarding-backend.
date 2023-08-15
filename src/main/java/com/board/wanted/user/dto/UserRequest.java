c980639c151799b311eac48350dfef39fa5e2979
yangsoohyun <cendy1234@naver.com>
2023-08-15 20:09:43 +0900

    feat: 사용자 로그인 엔드포인트 기능구현 (#4)

--- src/main/java/com/board/wanted/user/dto/UserRequest.java
+++ src/main/java/com/board/wanted/user/dto/UserRequest.java
@@ -26,4 +26,20 @@ public class UserRequest {
         private String password;
 
     }
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    @ToString
+    @Getter
+    public static class SignInDTO {
+
+        @NotBlank
+        @Email(regexp = ".*@.*", message = "유효하지 않은 이메일 형식입니다.")
+        private String email;
+
+        @NotBlank
+        @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
+        private String password;
+    }
 }
--- src/main/java/com/board/wanted/user/service/UserService.java
+++ src/main/java/com/board/wanted/user/service/UserService.java
@@ -2,10 +2,15 @@ package com.board.wanted.user.service;
 
 import com.board.wanted._core.errors.ErrorMessage;
 import com.board.wanted._core.errors.exception.Exception500;
+import com.board.wanted._core.security.JwtTokenProvider;
+import com.board.wanted._core.security.PrincipalUserDetail;
 import com.board.wanted.user.dto.UserRequest;
 import com.board.wanted.user.model.User;
 import com.board.wanted.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +20,7 @@ public class UserService {
 
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
+    private final AuthenticationManager authenticationManager;
 
     public void saveUser(UserRequest.UserDTO userDTO) {
         if (userDTO == null) throw new Exception500(ErrorMessage.EMPTY_DATA_TO_SIGNUP);
@@ -24,5 +30,15 @@ public class UserService {
                 .password(encodedPassword)
                 .build();
         userRepository.save(user);
+    }
+
+    public String signIn(UserRequest.SignInDTO signInDTO) {
+        if (signInDTO == null) throw new Exception500(ErrorMessage.EMPTY_DATA_TO_SIGNIN);
+        UsernamePasswordAuthenticationToken token
+                = new UsernamePasswordAuthenticationToken(signInDTO.getEmail(), signInDTO.getPassword());
+        Authentication authentication = authenticationManager.authenticate(token);
+        PrincipalUserDetail userDetail = (PrincipalUserDetail) authentication.getPrincipal();
+        final User user = userDetail.getUser();
+        return JwtTokenProvider.create(user);
     }
 }
